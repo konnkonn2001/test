e7af41ed138b0fb591c801e58461d73994767a76
konnkonn2001 <konnkonn.v.jh@gmail.com>
2025-10-05 00:37:46 +0900

    モック

--- my-vue-app/src/goraApi.js
+++ my-vue-app/src/goraApi.js
@@ -4,6 +4,29 @@ const API_KEY = '1067411969017260371';
 const BASE_URL = 'https://app.rakuten.co.jp/services/api/Gora/GoraGolfCourseSearch/20170623';
 
 export async function fetchGolfCourses(params = {}) {
+  // モックデータ返却
+  return {
+    Items: [
+      {
+        Item: {
+          golfCourseId: '10001',
+          golfCourseName: 'モックゴルフ場A',
+          address: '東京都千代田区1-1-1',
+          telephone: '03-1234-5678'
+        }
+      },
+      {
+        Item: {
+          golfCourseId: '10002',
+          golfCourseName: 'モックゴルフ場B',
+          address: '大阪府大阪市2-2-2',
+          telephone: '06-2345-6789'
+        }
+      }
+    ]
+  }
+  // 実API呼び出し（開発時はコメントアウト）
+  /*
   const query = new URLSearchParams({
     applicationId: API_KEY,
     ...params
@@ -17,4 +40,5 @@ export async function fetchGolfCourses(params = {}) {
     console.error(e);
     return null;
   }
+  */
 }
