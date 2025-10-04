82ac879e8759c6d8f63e41e464bd7ee012677098
konnkonn2001 <konnkonn.v.jh@gmail.com>
2025-10-05 00:53:13 +0900

    修正

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/test/my-vue-app/',  // ← GitHub Pages の公開パスに合わせる
+  base: '/test/',   // ← ここを修正
 })
